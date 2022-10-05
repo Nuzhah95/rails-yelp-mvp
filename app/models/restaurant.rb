3897cdb3b013d2a50280db2d8e0e00d28205e39d
Nuzhah Kureemun <nuzk2904@gmail.com>
2022-10-05 12:31:51 +0400

    rails-yelp-mvp

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
+
   has_many :reviews
-  validates :name, :address, :category
-  restaurant.save
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
